5869871f40e09b6902c06aee72de0c5eea57ad5a
Magatte <magattestg@gmail.com>
2018-09-28 19:32:02 +0200

    complete tic-tac toe game

--- src/components/board.js
+++ src/components/board.js
@@ -2,12 +2,34 @@ import React from 'react';
 import Square from './square.js';
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+    };
+
+    handleClick(i) {
+        const squares = this.state.squares.slice(); // Make copy of squares
+        squares[i] = this.state.xIsNext ? 'X' : 'O'; 
+        this.setState({
+            squares: squares, // Set the new square
+            xIsNext: !this.state.xIsNext // Set the next player
+        });
+    }
+
     renderSquare(i) {
-        return <Square value={i}/>;
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}    
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
