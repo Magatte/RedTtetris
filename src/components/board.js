782d1e6d2794801b24176a5295741537af0ecfd9
Magatte BA <pba@e3r5p20.42.fr>
2018-10-06 04:55:31 +0200

    shifting to functional component

--- src/components/board.js
+++ src/components/board.js
@@ -1,72 +1,48 @@
 import React from 'react';
 import Square from './square.js';
 
-class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(20).fill(null).map(()=>Array(10).fill(null)),
-            tetriminos: Array(4).fill(null),
-            start: {x: 0, y: 4},
-            pos: {x: 1, y: 4},
-        };
-    };
+function Board(props) {
 
-    showTetriminos(x, y) {
-        const squares = this.state.squares.slice(); // Make copy of squares
-        squares[x][y] = (x === this.state.pos.x - 1 && y === this.state.pos.y ? '#51B2E8' : '#fff');
-        this.setState({
-            squares: squares, // Set the new square
-        });
-    };
-
-    renderSquare(x, y) {
+    alert(props.pos.x);
+    function renderSquare(x, y, key) {
         return (
             <Square
-                value={this.state.pos.x -1}
-                color= {x === this.state.pos.x - 1 && y === this.state.pos.y ? '#51B2E8' : '#fff'}
+                key={key}
+                value={props.pos.x - 1}
+                color= {x === props.pos.x - 1 && y === props.pos.y ? '#51B2E8' : '#fff'}
                 onChange={() => this.showTetriminos(x, y)}
-                // color= {x === this.state.pos.x && y === this.state.pos.y ? '#51B2E8' : '#fff'} 
             />
         );
     };
 
-    createBoard() {
+    function createBoard() {
         const b = new Array(20).fill(null).map(() => Array(10).fill(null));
 
         const board = b.map((row, j) => {
+            let rowKey = j;
             return (
-                <div className='board-row'>
-                    {row.map((curr, i) => {return this.renderSquare(j, i)})}
+                <div className='board-row' key={rowKey}>
+                    {row.map((curr, i) => {
+                        let squareKey = rowKey * 10 + i;
+                        return renderSquare(j, i, squareKey);
+                    })}
                 </div>
             );
         });
         return board;
     };
 
-    componentDidMount() {
-        this.interval = setInterval(() => 
-            this.setState({ pos: {x: (this.state.pos.x % 20) + 1, y: 4}}),
-            1000
-        );
-    }
+    const status = 'Player 1';
     
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
-    render() {
-        const status = 'Player 1';
-    
-        return (
+    return (
+        <div>
+            <div className='status'>{status}</div>
             <div>
-                <div className='status'>{status}</div>
-                <div>
-                    {this.createBoard()}
-                </div>
+                {createBoard()}
             </div>
-        );
-    };
+        </div>
+    );
+
 };
 
 export default Board;
--- src/components/game.js
+++ src/components/game.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import Board from './board.js';
 
-class Game extends React.Component {
-    render() {
-        return (
-            <div className='game'>
-                <div className='game-board'>
-                    <Board />
-                </div>
-                <div className='game-info'>
-                    <div>{/* TODO */}</div>
-                    <ol> {/* TODO */} </ol>
-                </div>
+function Game(props) {
+    
+    const squares = Array(20).fill(null).map(()=>Array(10).fill(null));
+    var pos = {x: 1, y: 4};
+
+    function showTetriminos() {
+        pos.x = pos.x % 20 + 1;
+        return pos;
+    }
+
+    setInterval(showTetriminos, 1000);
+
+    return (
+        <div className='game'>
+            <div className='game-board'>
+                <Board 
+                    squares={squares}
+                    tetriminos={Array(4).fill(null)}
+                    start={{x: 0, y: 4}}
+                    pos={pos}
+                />
             </div>
-        );
-    };
+            <div className='game-info'>
+                <div>{/* TODO */}</div>
+                <ol> {/* TODO */} </ol>
+            </div>
+        </div>
+    );
 };
 
 export default Game;
