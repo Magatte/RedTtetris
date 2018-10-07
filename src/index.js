99e54d7693132bd8d8bb9cec47424effb64d34f8
Magatte BA <pba@e1r11p8.42.fr>
2018-10-07 02:54:59 +0200

    create js directory to handle redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './components/game.js';
 import './index.css';
+import index from './js/index';
 
 ReactDOM.render(
     <Game />,
