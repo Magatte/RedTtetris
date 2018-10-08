a14051b1ea0fbb0acb6661272fa022b5388efb2b
Magatte BA <pba@e1r8p11.42.fr>
2018-10-09 00:04:53 +0200

    did prototype newTetriminos action

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -10,6 +10,11 @@ export const FREEZE_LINE = 'FREEZE_LINE';
 export const MOVE_DOWN = 'MOVE_DOWN';
 export const MOVE_RIGHT = 'MOVE_RIGHT';
 export const MOVE_LEFT = 'MOVE_LEFT';
+export const NEW_TETRIMINOS = 'NEW_TETRIMINOS';
+
+export const newTetriminos = () => {
+
+};
 
 export const startGame = () => {
 
