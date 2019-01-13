c52822010f8a136e975c28a8a06e71d758fc99a3
Magatte BA <pba@e1r11p17.42.fr>
2019-01-13 02:45:57 +0100

    added Game, Games and Player classes

---
+++ server/controllers/Player.js
@@ -0,0 +1,16 @@
+export default class Player {
+
+    constructor(login, room) {
+        this.login = login
+        this.rooms = room
+    }
+
+    addRoom(room) {
+        return this.rooms.push(room)
+    }
+
+    getRooms() {
+        return this.rooms
+    }
+
+}
