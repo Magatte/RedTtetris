a25cfd57c1f300259d5783853071e36a6a85728e
magatte <magattestg@gmail.com>
2019-03-05 07:25:37 +0100

    fixing freeze line

--- client/src/redux/middleware/socketMiddleware.js
+++ client/src/redux/middleware/socketMiddleware.js
@@ -17,114 +17,118 @@ import {
 
 
 export default function socketMiddleware(socket) {
-    return ({ dispatch, getState }) => next => (action) => {
-
-        if (!socket) {
-            socket.emit('connection', 'hello je suis connecte');
-            return;
-        }
-
-        socket.on('start', (data) => {
-            console.log('dans start', data)
-        });
-
-        socket.on('status', (data) => {
+    return ({ dispatch, getState }) => {
+        
+        socket.on('freezeLine', data => {
             const action = {
-                type: DATA_FROM_SOCKET,
-                data
+                type: FREEZE_LINE
             }
-            console.log('DATA FROM SOCKET');
-            return next(action);
+            console.log('I WANT TO FREEZE');
+            return dispatch (action);
         });
 
-        // socket.on('freezeLine', data => {
-        //     const action = {
-        //         type: FREEZE_LINE
-        //     }
-        //     console.log('I WANT TO FREEZE');
-        //     return next(action);
-        // });
+        return next => (action) => {
 
-        const {
-            type,
-            room
-        } = action;
+            if (!socket) {
+                socket.emit('connection', 'hello je suis connecte');
+                return;
+            }
 
-        switch (type) {
-            case SEND_START_GAME: {
-                const data = {
-                    type,
-                    room
+            socket.on('start', (data) => {
+                console.log('dans start', data)
+            });
+
+            socket.on('status', (data) => {
+                const action = {
+                    type: DATA_FROM_SOCKET,
+                    data
                 }
-                socket.emit('gameStatus', data);
-                break ;
-            }
-            case PAUSE_GAME: {
-                const data = {
-                    type,
-                }
-                socket.emit('gameStatus', data);
-                break ;
-            }
-            case UNPAUSE_GAME:
-            case SEND_LOGIN_ROOM: {
-                socket.emit('userData', action.login, action.room)
-                break ;
-            }
-            case MANAGE_PIECES_STOCK: {
-                // When a user pieces stock is under 6 he sends a request to the server to get new pieces
-                socket.on('getNewPieces', (newPieces, room) => {
-                    const action = {
-                        type: NEW_PIECES_FROM_SOCKET,
-                        newPieces: newPieces,
+                console.log('DATA FROM SOCKET');
+                return next(action);
+            });
+
+
+            const {
+                type,
+                room
+            } = action;
+
+            switch (type) {
+                case SEND_START_GAME: {
+                    const data = {
+                        type,
                         room
                     }
-                    return next(action);
-                });
-                if (action.piecesStock.length < 5) {
-                    socket.emit('resquestShape', action.room);
+                    socket.emit('gameStatus', data);
+                    break ;
                 }
-                break ;
-            }
-            case GET_GAMES_LIST: {
-                socket.on('GamesList', (data) => {
-                    action = { ...action, games: data };
-                    return next(action);
-                });
-                break ;
-            }
-            case GET_PLAYER_STATUS: {
-                socket.on('playerStatus', (data) => {
-                    console.log('playerStatus', data)
-                    action = { ...action, data };
-
-                    return next(action);
-
-                });
-                break ;
-            }
-            case SEND_SPECTRE: {
-                socket.emit('sendSpectre', action.spectre, action.room, action.login);
-                socket.on('receiveSpectres', (room, allSpectres) => {
-                    const action = {
-                        type: RECEIVE_NEW_SPECTRE,
-                        room,
-                        allSpectres
+                case PAUSE_GAME: {
+                    const data = {
+                        type,
                     }
-                    return next(action);
-                });
-                break ;
+                    socket.emit('gameStatus', data);
+                    break ;
+                }
+                case UNPAUSE_GAME:
+                case SEND_LOGIN_ROOM: {
+                    socket.emit('userData', action.login, action.room)
+                    break ;
+                }
+                case MANAGE_PIECES_STOCK: {
+                    // When a user pieces stock is under 6 he sends a request to the server to get new pieces
+                    socket.on('getNewPieces', (newPieces, room) => {
+                        const action = {
+                            type: NEW_PIECES_FROM_SOCKET,
+                            newPieces: newPieces,
+                            room
+                        }
+                        return next(action);
+                    });
+                    if (action.piecesStock.length < 5) {
+                        socket.emit('resquestShape', action.room);
+                    }
+                    break ;
+                }
+                case GET_GAMES_LIST: {
+                    socket.on('GamesList', (data) => {
+                        action = { ...action, games: data };
+                        return next(action);
+                    });
+                    break ;
+                }
+                case GET_PLAYER_STATUS: {
+                    socket.on('playerStatus', (data) => {
+                        console.log('playerStatus', data)
+                        action = { ...action, data };
+
+                        return next(action);
+
+                    });
+                    break ;
+                }
+                case SEND_SPECTRE: {
+                    socket.emit('sendSpectre', action.spectre, action.room, action.login);
+                    socket.on('receiveSpectres', (room, allSpectres) => {
+                        const action = {
+                            type: RECEIVE_NEW_SPECTRE,
+                            room,
+                            allSpectres
+                        }
+                        return next(action);
+                    });
+                    break ;
+                }
+                case SEND_FREEZE_LINE: {
+                    socket.emit('sendFreezeLine', action.room, action.login);
+                    break ;
+                }
+                case STOP_GAME: {
+                    socket.emit('stopGame', action.room, action.login);
+                    break ;
+                }
+                default:
             }
-            case SEND_FREEZE_LINE: {
-                socket.emit('sendFreezeLine', action.room, action.login);
-                break ;
-            }
-            case STOP_GAME: {
-                socket.emit('stopGame', action.room, action.login);
-                break ;
-            }
-            default:
+            return next(action);
         }
-        return next(action);
     }
 }
